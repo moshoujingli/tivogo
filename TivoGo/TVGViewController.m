9afb545d755104e66fc0167559b4b4094d64d115
BiXiaopeng <bixiaopeng@BiXiaopengdeMacBook-Pro.local>
2013-11-21 01:43:32 +0900

    Initial Commit

---
+++ TivoGo/TVGViewController.m
@@ -0,0 +1,29 @@
+//
+//  TVGViewController.m
+//  TivoGo
+//
+//  Created by BiXiaopeng on 13-11-21.
+//  Copyright (c) 2013年 BiXiaopeng. All rights reserved.
+//
+
+#import "TVGViewController.h"
+
+@interface TVGViewController ()
+
+@end
+
+@implementation TVGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
